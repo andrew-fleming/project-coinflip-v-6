b11c590af037b0c279646441f4c2591452649505
andrew <fleming-andrew@protonmail.com>
2020-12-19 23:54:40 -0500

    Updates

---
+++ precache-manifest.96f7a065847d6a0ba174207d593d2059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe1c7908c5ee0f46f68c1cbd9537215",
+    "url": "/project-coinflip-v-6/index.html"
+  },
+  {
+    "revision": "a56d34278b9a899d542f",
+    "url": "/project-coinflip-v-6/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "11e2beb3e4fcdf767bac",
+    "url": "/project-coinflip-v-6/static/js/2.60ec43ab.chunk.js"
+  },
+  {
+    "revision": "103883be697ef591c817e5ef97e6a189",
+    "url": "/project-coinflip-v-6/static/js/2.60ec43ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56d34278b9a899d542f",
+    "url": "/project-coinflip-v-6/static/js/main.0557732a.chunk.js"
+  },
+  {
+    "revision": "a1d9ea927a180d8db3b6",
+    "url": "/project-coinflip-v-6/static/js/runtime-main.a8868bb3.js"
+  },
+  {
+    "revision": "914c7b7e087d081337467ed1824d282f",
+    "url": "/project-coinflip-v-6/static/media/backgroundImage.914c7b7e.jpg"
+  }
+]);
